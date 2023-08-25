b7a72399ad15c12bd93f93ffd4cfee10282e0a31
Gaurav Pratap Singh <gs492979@gmail.com>
2023-08-25 22:54:10 +0530

    Pages and Routes added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
+import { Offers } from "./pages/Offers";
+import { ForgotPassword } from "./pages/ForgotPassword";
+
 const App = () => {
-    return (
-        <h1 className=' font-bold text-4xl text-center my-1'>hello world</h1>
-    )
-}
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
-
-
-export default App
+export default App;
